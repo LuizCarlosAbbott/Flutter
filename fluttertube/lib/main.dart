8d16171a8e4945bbd74aaadf976168cd819cd119
luizcarlos <luizcarlos_abbott@hotmail.com>
2019-12-30 16:28:43 -0300

    iniciando flutter BLoC Pattern

---
+++ fluttertube/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertube/api.dart';
+import 'package:fluttertube/screens/home_screen.dart';
+
+void main(){
+  Api api = Api();
+  api.search("eletro");
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterTube',
+      home: Home(),
+    );
+  }
+}
+
